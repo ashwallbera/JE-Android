ecaaa5ff5e2cac175f44d4eb944e1cbbb6f47430
Jefferson Jadoc <70456980+ashwallbera@users.noreply.github.com>
2022-06-11 21:27:06 +0800

    [MOD] Change layout name

--- app/src/main/java/com/example/je_attendancesystem/fragments/FragmentTimesheet.java
+++ app/src/main/java/com/example/je_attendancesystem/fragments/FragmentTimesheet.java
@@ -75,7 +75,7 @@ public class FragmentTimesheet extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_calendar, container, false);
+        View view = inflater.inflate(R.layout.fragment_timesheet, container, false);
 
         //get the project object
         String objFromCard = getArguments().getString("projectObj");
