84b63d5cd0663595d69755c472885ea41a6c0f00
Jefferson Jadoc <70456980+ashwallbera@users.noreply.github.com>
2022-06-23 12:16:13 +0800

    [MOD] Add Properties

--- app/src/main/java/com/example/je_attendancesystem/models/TimesheetModel.java
+++ app/src/main/java/com/example/je_attendancesystem/models/TimesheetModel.java
@@ -1,4 +1,14 @@
 package com.example.je_attendancesystem.models;
 
 public class TimesheetModel {
+    public String userid;
+    public String id;
+    public String timeIn;
+    public String timeOut;
+    public String datecreated;
+
+    public TimesheetModel(){
+
+    }
+
 }
