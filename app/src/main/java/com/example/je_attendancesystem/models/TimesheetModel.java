7abf80826760e2b0f270b6c7bf39bf7a7a574f08
Jefferson Jadoc <70456980+ashwallbera@users.noreply.github.com>
2022-06-24 19:11:21 +0800

    [MOD] Add Getter methods

--- app/src/main/java/com/example/je_attendancesystem/models/TimesheetModel.java
+++ app/src/main/java/com/example/je_attendancesystem/models/TimesheetModel.java
@@ -2,7 +2,7 @@ package com.example.je_attendancesystem.models;
 
 public class TimesheetModel {
     public String userid;
-    public String id;
+    public String projectid;
     public String timeIn;
     public String timeOut;
     public String datecreated;
@@ -11,4 +11,23 @@ public class TimesheetModel {
 
     }
 
+    public String getProjectid() {
+        return projectid;
+    }
+
+    public String getUserid() {
+        return userid;
+    }
+
+    public String getTimeIn() {
+        return timeIn;
+    }
+
+    public String getTimeOut() {
+        return timeOut;
+    }
+
+    public String getDatecreated() {
+        return datecreated;
+    }
 }
