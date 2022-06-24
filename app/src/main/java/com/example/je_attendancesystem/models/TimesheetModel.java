984fe1983569ff3f16f949749d4354ee6ab13599
Jefferson Jadoc <70456980+ashwallbera@users.noreply.github.com>
2022-06-24 19:24:08 +0800

    [MOD] add overload constructor in timesheet model

--- app/src/main/java/com/example/je_attendancesystem/models/TimesheetModel.java
+++ app/src/main/java/com/example/je_attendancesystem/models/TimesheetModel.java
@@ -1,6 +1,7 @@
 package com.example.je_attendancesystem.models;
 
 public class TimesheetModel {
+    public String id;
     public String userid;
     public String projectid;
     public String timeIn;
@@ -9,6 +10,14 @@ public class TimesheetModel {
 
     public TimesheetModel(){
 
+    }
+    public TimesheetModel(String id, String projectid, String userid, String timeIn, String timeOut, String datecreated){
+        this.userid = userid;
+        this.id = id;
+        this.projectid = projectid;
+        this.timeIn = timeIn;
+        this.timeOut = timeOut;
+        this.datecreated = datecreated;
     }
 
     public String getProjectid() {
@@ -29,5 +38,9 @@ public class TimesheetModel {
 
     public String getDatecreated() {
         return datecreated;
+    }
+
+    public String getId() {
+        return id;
     }
 }
