7cc60d827f415bd87d0320906e8f2e38ea5bf1b5
Jefferson Jadoc <70456980+ashwallbera@users.noreply.github.com>
2022-06-25 12:42:44 +0800

    [MOD] Cou nter number of people in specific date

--- app/src/main/java/com/example/je_attendancesystem/adapter/ProjectAdapter.java
+++ app/src/main/java/com/example/je_attendancesystem/adapter/ProjectAdapter.java
@@ -1,6 +1,7 @@
 package com.example.je_attendancesystem.adapter;
 
 import android.content.Context;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,6 +11,7 @@ import android.view.animation.AnimationUtils;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.je_attendancesystem.R;
@@ -17,8 +19,15 @@ import com.example.je_attendancesystem.activity.MainMenu;
 import com.example.je_attendancesystem.fragments.FragmentTimesheet;
 import com.example.je_attendancesystem.models.ProjectModel;
 import com.google.android.material.card.MaterialCardView;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.google.gson.Gson;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 
 public class ProjectAdapter extends RecyclerView.Adapter {
@@ -66,12 +75,14 @@ public class ProjectAdapter extends RecyclerView.Adapter {
     public class ProjectAdapterHolder extends  RecyclerView.ViewHolder{
         MaterialCardView card;
         TextView project_name, total, location;
+        DatabaseReference mDatabase;
         public ProjectAdapterHolder(@NonNull View itemView) {
             super(itemView);
             project_name = itemView.findViewById(R.id.project_name);
             location = itemView.findViewById(R.id.project_location);
             total = itemView.findViewById(R.id.total_workers);
             card = itemView.findViewById(R.id.card);
+            mDatabase = FirebaseDatabase.getInstance().getReference();
 
         }
         void bind(ProjectModel projectModel){
@@ -81,12 +92,33 @@ public class ProjectAdapter extends RecyclerView.Adapter {
             card.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
-
                     Bundle bundle = new Bundle();
                     bundle.putString("projectObj", ""+new Gson().toJson(projectModel));
                     FragmentTimesheet timesheet = new FragmentTimesheet();
                     timesheet.setArguments(bundle);
                     ((MainMenu)context).replaceFragment(timesheet);
+                }
+            });
+
+            mDatabase.child("attendance").addValueEventListener(new ValueEventListener() {
+                @RequiresApi(api = Build.VERSION_CODES.O)
+                @Override
+                public void onDataChange(@NonNull DataSnapshot snapshot) {
+                    DateTimeFormatter dtf = DateTimeFormatter.ofPattern("M/dd/yyyy");
+                    String localDate = LocalDate.now().format(dtf);
+                    int x = 0;
+                    for(DataSnapshot dataSnapshot: snapshot.getChildren()){
+                        if(dataSnapshot.child("datecreated").getValue().toString().equals(localDate) &&
+                        dataSnapshot.child("projectid").getValue().toString().equals(""+projectModel.getId())){
+                            x++;
+                        }
+                    }
+                    total.setText(x+"/"+30);
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError error) {
+
                 }
             });
         }
