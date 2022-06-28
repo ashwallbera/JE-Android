0baa12c67629e9ecdcb009180fd7e2af2c5b857a
Jefferson Jadoc <70456980+ashwallbera@users.noreply.github.com>
2022-06-28 20:58:31 +0800

    [MOD] Post request for attendance

--- app/src/main/java/com/example/je_attendancesystem/activity/MainMenu.java
+++ app/src/main/java/com/example/je_attendancesystem/activity/MainMenu.java
@@ -14,16 +14,35 @@ import androidx.fragment.app.FragmentTransaction;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 import android.widget.Toast;
 
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.DefaultRetryPolicy;
+import com.android.volley.NetworkResponse;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.HttpHeaderParser;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.je_attendancesystem.R;
+import com.example.je_attendancesystem.api.Api;
 import com.example.je_attendancesystem.fragments.FragmentTimesheet;
 import com.example.je_attendancesystem.fragments.FragmentProject;
+import com.example.je_attendancesystem.models.TimesheetModel;
 import com.google.android.material.navigation.NavigationView;
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.UnsupportedEncodingException;
 
 public class MainMenu extends AppCompatActivity {
     private DrawerLayout drawerLayout;
@@ -117,6 +136,7 @@ public class MainMenu extends AppCompatActivity {
             AlertDialog.Builder builder = new AlertDialog.Builder(MainMenu.this);
             builder.setTitle("Result");
             builder.setMessage(intentResult.getContents());
+
             builder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
                 @Override
                 public void onClick(DialogInterface dialogInterface, int i) {
@@ -127,6 +147,67 @@ public class MainMenu extends AppCompatActivity {
         }
         else{
             Toast.makeText(getApplicationContext(), "OOPS ERROR", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
+    private void createAttendance(TimesheetModel model){
+        try {
+            RequestQueue requestQueue = Volley.newRequestQueue(this);
+            String URL = Api.server+"api/attendances";
+            JSONObject jsonBody = new JSONObject();
+            jsonBody.put("Title", "Android Volley Demo");
+            jsonBody.put("Author", "BNK");
+            final String requestBody = jsonBody.toString();
+
+            StringRequest stringRequest = new StringRequest(Request.Method.POST, URL, new Response.Listener<String>() {
+                @Override
+                public void onResponse(String response) {
+                    Log.i("VOLLEY", response);
+                }
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    Log.e("VOLLEY", error.toString());
+                }
+            }) {
+                @Override
+                public String getBodyContentType() {
+                    return "application/json; charset=utf-8";
+                }
+
+                @Override
+                public byte[] getBody() throws AuthFailureError {
+                    try {
+                        return requestBody == null ? null : requestBody.getBytes("utf-8");
+                    } catch (UnsupportedEncodingException uee) {
+                        VolleyLog.wtf("Unsupported Encoding while trying to get the bytes of %s using %s", requestBody, "utf-8");
+                        return null;
+                    }
+                }
+
+                @Override
+                protected Response<String> parseNetworkResponse(NetworkResponse response) {
+                    String responseString = "";
+                    if (response != null) {
+                        responseString = String.valueOf(response.statusCode);
+                        // can get more details such as response.headers
+                    }
+                    return Response.success(responseString, HttpHeaderParser.parseCacheHeaders(response));
+                }
+            };
+
+
+            // To retry if connection is slow
+            stringRequest.setShouldCache(false);
+            stringRequest.setRetryPolicy(new DefaultRetryPolicy(30000,
+                    DefaultRetryPolicy.DEFAULT_MAX_RETRIES,
+                    DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
+
+            // Add the request to the RequestQueue.
+            requestQueue.add(stringRequest);
+        } catch (JSONException e) {
+            e.printStackTrace();
         }
     }
 }
--- app/src/main/java/com/example/je_attendancesystem/api/Api.java
+++ app/src/main/java/com/example/je_attendancesystem/api/Api.java
@@ -1,4 +1,5 @@
 package com.example.je_attendancesystem.api;
 
 public class Api {
+    public static String server = "http://ashwallbera-001-site1.ctempurl.com";
 }
